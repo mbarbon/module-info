574fd17a1ae58bab75a7e8bcdc42b82f005555b0
Michael G. Schwern <schwern@pobox.com>
2001-12-10 17:16:41 +0000

    Version 0.03
    implemented modules_used.

--- lib/B/Module/Info.pm
+++ lib/B/Module/Info.pm
@@ -1,7 +1,10 @@
 package B::Module::Info;
 
+$VERSION = '0.03';
+
 use B;
-use B::Utils qw(walkoptree_filtered opgrep all_roots);
+use B::Utils qw(walkoptree_filtered walkoptree_simple
+                opgrep all_roots);
 
 sub state_change {
     return opgrep {name => [qw(nextstate dbstate setstate)]}, @_
@@ -24,10 +27,36 @@ my %modes = (
              },
              subroutines => sub {
                  my %roots = all_roots;
-                 print join "\n", keys %roots;
-             }
+                 while( my($name, $op) = each %roots ) {
+                     next if $name eq '__MAIN__';
+                     local($File, $Start, $End);
+                     walkoptree_simple($op, \&sub_info);
+                     print "$name at $File from $Start to $End\n";
+                 }
+             },
+             modules_used => sub {
+                 walkoptree_filtered(B::main_root,
+                                     \&is_begin,
+                                     \&begin_is_use );
+             },
             );
 
+sub sub_info {
+    $File  ||= $B::Utils::file;
+    $Start = $B::Utils::line if !$Start || $B::Utils::line < $Start;
+    $End   = $B::Utils::line if !$End   || $B::Utils::line > $End;
+}
+
+sub is_begin {
+    my($op) = shift;
+    print $op->name;
+    return $op->name eq 'begin';
+}
+
+sub begin_is_use {
+    my($op) = shift;
+    print "Saw begin\n";
+}
 
 sub compile {
     my($mode) = shift;
