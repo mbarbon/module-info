26a0a5737c14bd3bd5dac8ff63839d8b63f0d776
Mattia Barbon <mbarbon@cpan.org>
2010-04-05 17:22:54 +0200

    Expand the revision tag, since Git does not do it.

--- t/lib/Boo.pm
+++ t/lib/Boo.pm
@@ -3,6 +3,6 @@ package Boo;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.35$ =~ /(\d+)\.(\d+)/);
 
 1;
